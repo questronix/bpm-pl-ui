df7f45949773ab3cc2a75a4b572234221645e27a
juliennesantos <juliennejsantos@gmail.com>
2018-09-25 14:14:18 +0800

    added sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './override.css';
+import Search from './modules/search/component/Search';
+import Navbar from './shared/component/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar/>
+        <Search/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
--- src/shared/component/Navbar.js
+++ src/shared/component/Navbar.js
@@ -1,1 +1,17 @@
-//
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+          <p>
+              This is a search
+          </p>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
