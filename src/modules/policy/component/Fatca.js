c142a28158424aa2117e59ab2e030968b676b252
hakunozen <sirjeromepatiga@gmail.com>
2018-09-28 09:14:55 +0800

    added custom radio button

--- src/modules/policy/component/Fatca.js
+++ src/modules/policy/component/Fatca.js
@@ -101,129 +101,148 @@ class Fatca extends Component {
 
     const hh = this.state.hazardousHobbies;
     hh.forEach((hobby) => {
-      if (""+hobby.id === name) {
+      if ("" + hobby.id === name) {
         hobby.value = checked;
       }
     });
 
     this.setState({ hazardousHobbies: hh });
   }
- 
+
   render() {
     const hobbies = this.state.hazardousHobbies
       .map((hobby, i) => {
         return <div key={i}>
-          <input 
+          <input
             key={hobby.id}
-            name={hobby.id} 
-            checked={hobby.value} 
-            onChange={this.handleMultiSelectChange} 
-            type="checkbox"/> {hobby.label} <br/>
+            name={hobby.id}
+            checked={hobby.value}
+            onChange={this.handleMultiSelectChange}
+            type="checkbox" /> {hobby.label} <br />
         </div>
-    });
+      });
 
     return (
       <div className="App">
         <form onSubmit={this.handleSubmit}>
-          <div className="flex-containter flex-wrap col xl-6">
-            <div className="col xl-12 f-center f-justify-between flex f-center f-justify-between">
-              <label className="col xl-6">FATCA Tagging</label>
-              <input className="input col xl-6"
-                name="fatca"
-                value={this.state.fatca}
-                onChange={this.handleInputChange}
-                type="text" />
-            </div>
-            <div className="col xl-12 f-center f-justify-between flex f-center f-justify-between">
-              <label className="col xl-6">Place of Signing within the Philippines:</label>
-              <input className="input col xl-6"
-                name="placeOfSigning"
-                value={this.state.placeOfSigning}
-                onChange={this.handleInputChange}
-                type="text" />
-            </div>
-            <div className="col xl-12 f-center f-justify-between flex f-center f-justify-between">
-              <label className="col xl-6">Statement of Insurability:</label>
-              <input className="input col xl-6"
-                name="statementOfInsurablity"
-                value={this.state.statementOfInsurablity}
-                onChange={this.handleInputChange}
-                type="text" />
-            </div>
-            <div className="col xl-12 f-center f-justify-between flex f-center f-justify-between">
-              <label className="col xl-6">With Diabetes:</label>
-              <input className="input col"
-                name="withDiabetes"
-                value="false"
-                type="radio"
-                checked={this.state.withDiabetes === false}
-                onChange={this.handleRadioChange} /> No
+          <div className="flex-containter flex-wrap col xl-12">
+            <div className="card col xl-12 l-12 m-12 s-12 xs-12 no-padding">
+              <div className="card-header bg-prulife">
+                <h3 className="font-white no margin">
+                  Insured Information
+                </h3>
+              </div>
+              <div className="card-body">
+                <div className="col xl-12 flex f-center f-justify-space-between">
+                  <label>FATCA Tagging</label>
+                  <input className="input col xl-6"
+                    name="fatca"
+                    value={this.state.fatca}
+                    onChange={this.handleInputChange}
+                    type="text" />
+                </div>
+                <div className="col xl-12 flex f-center f-justify-space-between">
+                  <label>Place of Signing within the Philippines:</label>
+                  <input className="input col xl-6"
+                    name="placeOfSigning"
+                    value={this.state.placeOfSigning}
+                    onChange={this.handleInputChange}
+                    type="text" />
+                </div>
+                <div className="col xl-12 flex f-center f-justify-space-between">
+                  <label>Statement of Insurability:</label>
+                  <input className="input col xl-6"
+                    name="statementOfInsurablity"
+                    value={this.state.statementOfInsurablity}
+                    onChange={this.handleInputChange}
+                    type="text" />
+                </div>
+                <div>
+                  <label>With Diabetes:</label>
+                  <input className="input col"
+                    name="withDiabetes"
+                    value="false"
+                    type="radio"
+                    checked={this.state.withDiabetes === false}
+                    onChange={this.handleRadioChange} /> No
+                  <input className="input col"
+                    name="withDiabetes"
+                    value="true"
+                    checked={this.state.withDiabetes === true}
+                    onChange={this.handleRadioChange}
+                    type="radio" /> Yes
+                    <div className="col xl-12 flex f-center f-justify-space-between">
+                    <div class="can-toggle can-toggle--size-small">
+                      <input id="b" type="checkbox" />
+                      <label for="b">
+                        <div class="can-toggle__switch" data-checked="Yes" data-unchecked="No"></div>
+                        <div class="can-toggle__label-text">With Diabetes:</div>
+                      </label>
+                    </div>
+                  </div>
+
+                </div>
+                <div>
+                  <label>With Hypertension:</label>
+                  <input className="input col"
+                    name="withHypertension"
+                    value="false"
+                    checked={this.state.withHypertension === false}
+                    onChange={this.handleRadioChange}
+                    type="radio" /> No
             <input className="input col"
-                name="withDiabetes"
-                value="true"
-                checked={this.state.withDiabetes === true}
-                onChange={this.handleRadioChange}
-                type="radio" /> Yes
+                    name="withHypertension"
+                    value="true"
+                    checked={this.state.withHypertension === true}
+                    onChange={this.handleRadioChange}
+                    type="radio" /> Yes
               </div>
-            <div className="col xl-12 f-center f-justify-between flex f-center f-justify-between">
-              <label className="col xl-6">With Hypertension:</label>
-              <input className="input col"
-                name="withHypertension"
-                value="false"
-                checked={this.state.withHypertension === false}
-                onChange={this.handleRadioChange}
-                type="radio" /> No
+                <div>
+                  <label>With Pregnancy:</label>
+                  <input className="input col"
+                    name="withPregnancy"
+                    value="false"
+                    checked={this.state.withPregnancy === false}
+                    onChange={this.handleRadioChange}
+                    type="radio" /> No
             <input className="input col"
-                name="withHypertension"
-                value="true"
-                checked={this.state.withHypertension === true}
-                onChange={this.handleRadioChange}
-                type="radio" /> Yes
+                    name="withPregnancy"
+                    value="true"
+                    checked={this.state.withPregnancy === true}
+                    onChange={this.handleRadioChange}
+                    type="radio" /> Yes
               </div>
-            <div className="col xl-12 f-center f-justify-between flex f-center f-justify-between">
-              <label className="col xl-6">With Pregnancy:</label>
-              <input className="input col"
-                name="withPregnancy"
-                value="false"
-                checked={this.state.withPregnancy === false}
-                onChange={this.handleRadioChange}
-                type="radio" /> No
-            <input className="input col"
-                name="withPregnancy"
-                value="true"
-                checked={this.state.withPregnancy === true}
-                onChange={this.handleRadioChange}
-                type="radio" /> Yes
+                <div>
+                  <label>Flight Stewardess/Steward/Pilot/Purser:</label>
+                  <input className="input col xl-6"
+                    name="flightSSP"
+                    value={this.state.flightSSP}
+                    onChange={this.handleInputChange}
+                    type="text" /> No
               </div>
-            <div className="col xl-12 f-center f-justify-between flex f-center f-justify-between">
-              <label className="col xl-6">Flight Stewardess/Steward/Pilot/Purser:</label>
-              <input className="input col xl-6"
-                name="flightSSP"
-                value={this.state.flightSSP}
-                onChange={this.handleInputChange}
-                type="text" /> No
+                <div className="col xl-12 flex f-justify-between flex-container flex-wrap f-center">
+                  <h4 className="col xl-12">Hazardous Hobbies:</h4><br />
+                  <label className="col xl-6">Bungee Jumping</label>
+                  <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
+                  <label className="col xl-6">Hang Gliding</label>
+                  <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
+                  <label className="col xl-6">Horse Racing</label>
+                  <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
+                  <label className="col xl-6">Motor Racing</label>
+                  <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
+                  <label className="col xl-6">Mountain Climbing</label>
+                  <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
+                  <label className="col xl-6">Parachuting</label>
+                  <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
+                  <label className="col xl-6">Private Pilot (includig Gliding)</label>
+                  <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
+                  <label className="col xl-6">Scuba Diving</label>
+                  <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
+                  <label className="col xl-6">Sky Diving</label>
+                  <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
+                  <input className="btn primary col xl-12 " type="submit" value="submit" />
+                </div>
               </div>
-            <div className="col xl-12 flex f-justify-between flex-container flex-wrap f-center">
-              <h4 className="col xl-12">Hazardous Hobbies:</h4><br />
-              <label className="col xl-6">Bungee Jumping</label>
-              <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
-              <label className="col xl-6">Hang Gliding</label>
-              <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
-              <label className="col xl-6">Horse Racing</label>
-              <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
-              <label className="col xl-6">Motor Racing</label>
-              <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
-              <label className="col xl-6">Mountain Climbing</label>
-              <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
-              <label className="col xl-6">Parachuting</label>
-              <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
-              <label className="col xl-6">Private Pilot (includig Gliding)</label>
-              <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
-              <label className="col xl-6">Scuba Diving</label>
-              <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
-              <label className="col xl-6">Sky Diving</label>
-              <input className="input col xl-6 " name="hazardousHobbies" type="checkbox" />
-              <input className="btn primary col xl-12 " type="submit" value="submit" />
             </div>
           </div>
         </form>
--- src/modules/policy/component/Transaction.js
+++ src/modules/policy/component/Transaction.js
@@ -1,47 +1,70 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Transaction extends Component {
   render() {
     const actions = this.props.transaction.checkList
       .map((action, i) => {
-        return <div key={i}>
-          <input 
-            key={action.id}
-            name={action.id} 
-            // checked={action.value} 
-            // onChange={this.handleMultiSelectChange} 
-            type="checkbox"/> {action.label} <br/>
+        return <div className="col xl-12" key={i}>
+          <label className="container">{action.label}
+            <input
+              key={action.id}
+              name={action.id}
+              // checked={action.value} 
+              // onChange={this.handleMultiSelectChange} 
+              type="checkbox" />
+              <span className="checkmark"></span>
+          </label>
         </div>
-    });
+      });
 
     return (
       <div>
-        <h1>Transaction</h1>
-        <label>TXN {this.props.transaction.number}</label>
-        <label>Transaction Type: </label>
-        <select>
-          <option >Reinstatement</option>
-        </select>
-        <label>Transaction Type: </label>
-        <input 
-          name="action"
-          value="false"
-          // checked={this.state.withHypertension === false}
-          onChange={this.handleRadioChange}
-          type="radio"/> Updating
-        <input 
-          name="action"
-          value="false"
-          // checked={this.state.withHypertension === false}
-          onChange={this.handleRadioChange}
-          type="radio"/> Redating
-        <input 
-          name="action"
-          value="false"
-          // checked={this.state.withHypertension === false}
-          onChange={this.handleRadioChange}
-          type="radio"/> Waiver
-          {actions}
+        <div className="flex-container flex-wraper">
+          <div className="col xl-12">
+            <div className="card col xl-12 l-12 m-12 s-12 xs-12 no-padding">
+              <div className="card-header bg-prulife flex f-justify-space-between">
+                <div className="col xl-6 flex">
+                  <h3 className="font-white">Transaction type:</h3>
+                  <select className="select-no-effect font-white bg-none no-border h3">
+                    <option className="hidden">Reinstatement</option>
+                    <option className="item">Reinstatement</option>
+                  </select>
+                </div>
+                <h3 className="font-white">TXN {this.props.transaction.number}</h3>
+              </div>
+              <div className="card-body flex-container flex-wrap">
+                <div className="col xl-6 flex f-center">
+                  <div class="switch-field flex f-column col xl-3">
+                    <input type="radio" id="switch_3_left"
+                      name="action"
+                      value="false"
+                      // checked={this.state.withHypertension === false}
+                      onChange={this.handleRadioChange}
+                      type="radio" />
+                    <label for="switch_3_left">Updating</label>
+                    <input type="radio" id="switch_3_center"
+                      name="action"
+                      value="false"
+                      // checked={this.state.withHypertension === false}
+                      onChange={this.handleRadioChange}
+                      type="radio" />
+                    <label for="switch_3_center">Rebating</label>
+                    <input type="radio" id="switch_3_right"
+                      name="action"
+                      value="false"
+                      // checked={this.state.withHypertension === false}
+                      onChange={this.handleRadioChange}
+                      type="radio" />
+                    <label for="switch_3_right">Waiver</label>
+                  </div>
+                </div>
+                <div className="col xl-6 flex-container flex-wrap">
+                  {actions}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
