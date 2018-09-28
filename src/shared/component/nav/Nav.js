1a265b0569ec9f364e31f4ba58daa980055fa5d2
mhackyu <markpaderes0932@yahoo.com>
2018-09-28 13:36:03 +0800

    Initial nav and sidebar component

---
+++ src/shared/component/nav/Nav.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Nav;
