9a058f53cb681f395f071d28eca82147d5724e9b
hakunozen <sirjeromepatiga@gmail.com>
2018-10-01 16:45:07 +0800

    modified search component

--- src/modules/policy/component/SearchPolicyForm.js
+++ src/modules/policy/component/SearchPolicyForm.js
@@ -29,12 +29,12 @@ class SearchPolicyForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <div className="flex-wrap flex-container">
           <label className="col xl-1 l-1 m-1 s-12 xs-12 flex f-center"> Policy Number: </label>
-          <input className="input xl-3 l-3 m-3 col s-9 xs-9"
+          <input className="input xl-4 l-4 m-4 col s-9 xs-9"
             type="text"
             placeholder="Input policy number here"
             value={this.state.policyNo}
             onChange={this.handleInputChange} />
-          <input className="btn bright-blue col xl-2 l-2 m-2 s-3 xs-3" type="submit" value="Search" />
+          <input className="btn bright-blue col xl-1 l-1 m-1 s-3 xs-3" type="submit" value="Search" />
         </div>
       </form>
     );
