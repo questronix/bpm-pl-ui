4ec588a14c2a11fd744813d9ca1fe1ee17fb624f
hakunozen <sirjeromepatiga@gmail.com>
2018-10-03 18:24:49 +0800

    added new loading animation

--- src/shared/component/loadings/PageLoading.js
+++ src/shared/component/loadings/PageLoading.js
@@ -4,14 +4,26 @@ class PageLoading extends Component {
   render() {
     return (
       <div className="App">
-        <div className="flex-container">
-          <div className="col xl-3 bg-lightgray">
-            <h3 className="invisible">
+        <div className="flex-container flex-wrap">
+          <div className="col xl-3 page-loading-header">
+            <h4 className="no-margin invisible">
               Placehonder Header
-            </h3>
+            </h4>
           </div>
-          <div className="card-body">
-            this is a card body
+          <div className="col xl-12 page-loading-body">
+            <p className="invisible">
+              this is a card body
+            </p>
+          </div>
+          <div className="col xl-3 page-loading-header">
+            <h4 className="no-margin invisible">
+              Placehonder Header
+            </h4>
+          </div>
+          <div className="col xl-12 page-loading-body">
+            <p className="invisible">
+              this is a card body
+            </p>
           </div>
         </div>
       </div>
