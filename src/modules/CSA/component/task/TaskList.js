0a4d75e26769c277d9019a2cbd16f9e51b360a58
hakunozen <sirjeromepatiga@gmail.com>
2018-10-12 11:55:08 +0800

    fixed table

--- src/modules/CSA/component/task/TaskList.js
+++ src/modules/CSA/component/task/TaskList.js
@@ -1,34 +1,38 @@
 import React from 'react';
+import CardTable from '../../../../shared/component/table/Table';
 
 const TaskList = (props) => (
-  <div className="card-table">
-    {props.tasks ? (
-      <table id="table" cellSpacing={0} cellPadding={0}>
-      <thead>
-        <tr>
-        <th>Process Name</th>
-        <th>Application No</th>
-        <th>Date Created</th>
-        <th>Status</th>
-        <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-      {props.tasks.map((task, index) => (
-        <tr key={index}>
-          {/* <Redirect to={`/tasks?id=${task.id}`} > Sample </Redirect> */}
-          <td>{task.name}</td>
-          <td>{task.policy.appNo}</td>
-          <td>{(new Date(task.policy.createdAt)).toDateString()}</td>
-          <td>{(task.completed) ?'Completed' : 'Draft'}</td>
-          <td>
-            {!task.completed ? (<a href={`/csa/tasks/edit?id=${task.id}`}>Edit</a>) : 'No Action'}
-          </td>
-        </tr>
-      ))}
-      </tbody>
-    </table>
-    ): 'No available tasks.' }
+  // <div className="card-table">
+  //   {props.tasks ? (
+  //     <table id="table" cellSpacing={0} cellPadding={0}>
+  //     <thead>
+  //       <tr>
+  //       <th>Process Name</th>
+  //       <th>Application No</th>
+  //       <th>Date Created</th>
+  //       <th>Status</th>
+  //       <th>Action</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>
+  //     {props.tasks.map((task, index) => (
+  //       <tr key={index}>
+  //         {/* <Redirect to={`/tasks?id=${task.id}`} > Sample </Redirect> */}
+  //         <td>{task.name}</td>
+  //         <td>{task.policy.appNo}</td>
+  //         <td>{(new Date(task.policy.createdAt)).toDateString()}</td>
+  //         <td>{(task.completed) ?'Completed' : 'Draft'}</td>
+  //         <td>
+  //           {!task.completed ? (<a href={`/csa/tasks/edit?id=${task.id}`}>Edit</a>) : 'No Action'}
+  //         </td>
+  //       </tr>
+  //     ))}
+  //     </tbody>
+  //   </table>
+  //   ): 'No available tasks.' }
+  // </div>
+  <div>
+    <CardTable/>
   </div>
 );
 
