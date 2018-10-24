6ac65f87bab0ea740533bb0a9a31582bf04f700c
hakunozen <sirjeromepatiga@gmail.com>
2018-10-24 15:22:05 +0800

    fixed processor edit page

--- src/modules/Processor/component/processor/PreScreening.js
+++ src/modules/Processor/component/processor/PreScreening.js
@@ -1,15 +1,59 @@
 import React from 'react';
 import CheckBox from '../../../../shared/component/checkbox/CheckBox';
-import TabBody from './TabBody';
+import CardImg from '../../../../shared/component/Widgets/CardImg';
 
 const PrescreeningInfo = (props) => {
   const insuredInfo = props.insuredInfo;
 
   return (
     <div>
-      <TabBody/>
+      <div className="xl-12 l-12 m-12 s-12 xs-12 flex-container flex-wrap tab-body">
+        <div className="s-f-column xs-f-column col xl-6 l-6 m-6 s-12 xs-12 flex f-column">
+          <label className="label">Policy Number :</label>
+          <input value={insuredInfo.policy.number} className="col input disabled" type="text" disabled />
+        </div>
+        <div className="s-f-column xs-f-column col xl-6 l-6 m-6 s-12 xs-12 flex f-column">
+          <label className="label">Product Type :</label>
+          <input value={insuredInfo.policy.sumAssured} className="col input disabled" type="text" disabled />
+        </div>
+        <div className="s-f-column xs-f-column col xl-6 l-6 m-6 s-12 xs-12 flex f-column">
+          <label className="label">Sum Assured :</label>
+          <input value={insuredInfo.policy.productType} className="col input disabled" type="text" disabled />
+        </div>
+        <CheckBox class=" col" label="FATCA Tagging :" cName="fatcaTagging" />
+        <div value={insuredInfo.policy.fatcaTagging} className="s-f-column xs-f-column col xl-6 l-6 m-6 s-12 xs-12 flex f-column">
+        </div>
+      </div>
+      <hr className="xl-11" />
+      <div className="header">
+        <h3 className="font-prulife">
+          Client
+        </h3>
+      </div>
+      <div className="xl-12 l-12 m-12 s-12 xs-12 flex-container flex-wrap tab-body">
+        <div className="s-f-column xs-f-column col xl-6 l-6 m-6 s-12 xs-12 flex f-column">
+          <label>Cleint type :</label>
+          <input value={insuredInfo.client.type} className="col input disabled" type="text" disabled />
+        </div>
+        <div className="s-f-column xs-f-column col xl-6 l-6 m-6 s-12 xs-12 flex f-column">
+          <label>Attain Age :</label>
+          <input value={insuredInfo.client.attainedAge} className="col input disabled" type="text" disabled />
+        </div>
+        <div className="s-f-column xs-f-column col xl-6 l-6 m-6 s-12 xs-12 flex f-column">
+          <label>Client Tagging :</label>
+          <input value={insuredInfo.client.clientTagging} className="col input disabled" type="text" disabled />
+        </div>
+        <div className="s-f-column xs-f-column col xl-6 l-6 m-6 s-12 xs-12 flex f-column">
+          <label>Statement of Insurability :</label>
+          <input value={insuredInfo.client.soi} className="col input disabled" type="text" disabled />
+        </div>
+        <CheckBox value={insuredInfo.client.mid} class=" col" label="MID :" cName="mid" />
+        <CheckBox value={insuredInfo.client.dowJones} class=" col" label="Dow Jones Hit :" cName="dowJonesHit" />
+        <div className="col xl-6 l-6 m-6 s-12 xs-12 flex f-center  m-f-center s-f-center xs-f-center f-justify-space-between">
+        </div>
+      </div>
     </div>
-      );
-    };
-    
+  );
+};
+
 export default PrescreeningInfo;
