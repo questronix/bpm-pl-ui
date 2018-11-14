f73b56e96ba57654e32da2c88960a19de4c74293
Bandaggao <ban_daggao@questronix.com.ph>
2018-11-14 22:29:13 +0800

    Bind remaining data

--- src/modules/CSA/component/policy/PolicyInformationNew.js
+++ src/modules/CSA/component/policy/PolicyInformationNew.js
@@ -23,14 +23,14 @@ class PolicyInformationNew extends Component {
         <Input editable="false" inputLabel="Premium Status:" inputPlaceholder="Premium Paying" txtboxClass="no-border" inputClass="col xl-3 input-container" value={this.props.policy.premiumStatus}/>
         <Input editable="false" inputLabel="First Issue Date:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-4 input-container" value={this.props.policy.firstIssueDate}/>
         <Input editable="false" inputLabel="Risk Commencement Date:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-4 input-container" value={this.props.policy.riskCommencementDate} />
-        <Input editable="false" inputLabel="Sum Assured:" inputPlaceholder="100,000" txtboxClass="no-border" inputClass="col xl-4 input-container" value=""/>
-        <Input editable="false" inputLabel="Currency:" inputPlaceholder="PHP" txtboxClass="no-border" inputClass="col xl-4 input-container" value=""/>
-        <Input editable="false" inputLabel="Current Premium:" inputPlaceholder="Current Premium" txtboxClass="no-border" inputClass="col xl-4 input-container" value="" />
+        <Input editable="false" inputLabel="Sum Assured:" inputPlaceholder="100,000" txtboxClass="no-border" inputClass="col xl-4 input-container" value={this.props.policy.sumInsured}/>
+        <Input editable="false" inputLabel="Currency:" inputPlaceholder="PHP" txtboxClass="no-border" inputClass="col xl-4 input-container" value={this.props.policy.currency}/>
+        <Input editable="false" inputLabel="Current Premium:" inputPlaceholder="Current Premium" txtboxClass="no-border" inputClass="col xl-4 input-container" value={this.props.policy.currentPremium} />
         <Input editable="false" inputLabel="Mode of Payment:" inputPlaceholder="Monthly" txtboxClass="no-border" inputClass="col xl-4 input-container" value="" />
-        <Input editable="false" inputLabel="Insured Name:" inputPlaceholder="LastN, FirstN, MName" txtboxClass="no-border" inputClass="col xl-8 input-container" value={this.props.policy.firstName}/>
-        <Input editable="false" inputLabel="Birthdate:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-4 input-container" value=""/>
-        <Input editable="false" inputLabel="Owner Name:" inputPlaceholder="LastN, FirstN, MName" txtboxClass="no-border" inputClass="col xl-8 input-container" value="" />
-        <Input editable="false" inputLabel="Birthdate:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-4 input-container" value=""/>
+        <Input editable="false" inputLabel="Insured Name:" inputPlaceholder="LastN, FirstN, MName" txtboxClass="no-border" inputClass="col xl-8 input-container" value={this.props.policy.clients[1].clientLastName + " " + this.props.policy.clients[1].clientFirstName + " " + this.props.policy.clients[1].clientMiddleName}/>
+        <Input editable="false" inputLabel="Birthdate:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-4 input-container" value={this.props.policy.clients[1].dateOfBirth}/>
+        <Input editable="false" inputLabel="Owner Name:" inputPlaceholder="LastN, FirstN, MName" txtboxClass="no-border" inputClass="col xl-8 input-container" value={this.props.policy.clients[0].clientLastName + " " + this.props.policy.clients[0].clientFirstName + " " + this.props.policy.clients[0].clientMiddleName}  />
+        <Input editable="false" inputLabel="Birthdate:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-4 input-container" value={this.props.policy.clients[0].dateOfBirth}/>
       </div>
 
     );
---
+++ src/modules/Processor/component/processor/ReviewTransaction.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Input from '../../../../shared/component/input/Input';
+import PolicyInfoHeader from '../../../CSA/component/policy/PolicyInfoHeader';
+import AgentInfoHeader from '../../../CSA/component/policy/AgentInfoHeader';
+
+class ReviewTransaction extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      policy: {},
+      visible: false
+    }
+    this.isVisible = this.isVisible.bind(this);
+  }
+  isVisible() {
+    this.setState({ visible: !this.state.visible })
+  }
+
+  render() {
+    return (
+      <div className="App processor-container">
+        <div className="showmore bg-bright-blue flex">
+          <button className="col flex f-justify-space-between no-border bg-none text-with-icon flex f-center" onClick={this.isVisible} accessKey="s">
+            <h3 className="font-white no-margin">
+            Policy Information
+            </h3>
+              &nbsp; <span className="fa fa-chevron-down font-white"></span>
+          </button>
+        </div>
+        <div className={this.state.visible ? "processor-dropdown active" : "processor-dropdown"}>
+          <div className="col xl-12">
+            <h2 className="font-prulife no-margin">
+              Policy Information
+            </h2>
+          </div>
+          <PolicyInfoHeader policy={this.props.policy} client={this.props.client} />
+          <div className="col xl-12">
+            <h2 className="font-prulife no-margin">
+              Agent Information
+            </h2>
+          </div>
+          <div className="col xl-12">
+            <h2 className="font-prulife no-margin">
+              Agent Information
+            </h2>
+          </div>
+          <div className="col xl-12">
+            <h2 className="font-prulife no-margin">
+              Agent Information
+            </h2>
+          </div>
+          <div className="col xl-12">
+            <h2 className="font-prulife no-margin">
+              Agent Information
+            </h2>
+          </div>
+          {/* <AgentInfoHeader /> */}
+        </div>
+      </div >
+    );
+  }
+}
+
+export default ReviewTransaction;
