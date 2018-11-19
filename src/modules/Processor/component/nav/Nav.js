99597989a72dc405860d0792fa63cf322c3b0282
Bandaggao <ban_daggao@questronix.com.ph>
2018-11-19 16:02:46 +0800

    Modified Nav files

--- src/modules/Processor/component/nav/Nav.js
+++ src/modules/Processor/component/nav/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from  '../../../../shared/component/nav/Nav';
+import NavBar from  '../../../../shared/component/nav/NavProcessor';
 
 const navLinks = [
   {
