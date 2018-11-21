227d6b3e82ee10362be8bef85242205ce8e9a713
Bandaggao <ban_daggao@questronix.com.ph>
2018-11-21 11:08:01 +0800

    Modified InfoHeader

--- src/modules/CSA/component/policy/PolicyInfoHeader.js
+++ src/modules/CSA/component/policy/PolicyInfoHeader.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import Input from '../../../../shared/component/input/Input';
 
 class PolicyInfoHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      isApprover: false
+    }
+  }
   render() {
     
 
@@ -16,6 +22,7 @@ class PolicyInfoHeader extends Component {
       console.log(owner, insured);
     }
     return (
+      this.state.isApprover ?
       <div className="xl-12 flex-container flex-wrap insured-details">
         <Input editable="false" inputLabel="Policy Number:" inputPlaceholder="0000001" txtboxClass="no-border" inputClass="col xl-3 input-container" value={policy.policyNo}/>
         <Input editable="false" inputLabel="Sum Assured:" inputPlaceholder="100,000" txtboxClass="no-border" inputClass="col xl-3 input-container" value={policy.sumInsured}/>
@@ -36,6 +43,27 @@ class PolicyInfoHeader extends Component {
         <Input editable="false" inputLabel="Birthdate:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-2 input-container" value={owner && owner.dateOfBirth}/>
       </div>
 
+      : 
+      
+      <div className="xl-12 flex-container flex-wrap insured-details">
+      <Input editable="false" inputLabel="Policy Number:" inputPlaceholder="0000001" txtboxClass="no-border" inputClass="col xl-3 input-container" value={policy.policyNo}/>
+      <Input editable="false" inputLabel="Sum Assured:" inputPlaceholder="100,000" txtboxClass="no-border" inputClass="col xl-3 input-container" value={policy.sumInsured}/>
+      <Input editable="false" inputLabel="Plan Status:" inputPlaceholder="Lapsed" txtboxClass="no-border" inputClass="col xl-3 input-container" value={policy.premiumStatus}/>
+      <Input editable="false" inputLabel="Premium Status:" inputPlaceholder="Premium Paying" txtboxClass="no-border" inputClass="col xl-3 input-container" value={policy.premiumStatus}/>
+
+      <Input editable="false" inputLabel="Plan Description:" inputPlaceholder="Plan Name" txtboxClass="no-border" inputClass="col xl-4 input-container" value={policy.planDescription}/>
+      <Input editable="false" inputLabel="First Issue Date:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-4 input-container" value={policy.firstIssueDate}/>
+      <Input editable="false" inputLabel="Risk Commencement Date:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-4 input-container" value={policy.riskCommencementDate} />
+
+      <Input editable="false" inputLabel="Currency:" inputPlaceholder="PHP" txtboxClass="no-border" inputClass="col xl-4 input-container" value={policy.currency}/>
+      <Input editable="false" inputLabel="Current Premium:" inputPlaceholder="Current Premium" txtboxClass="no-border" inputClass="col xl-4 input-container" value={policy.currentPremium} />
+      <Input editable="false" inputLabel="Mode of Payment:" inputPlaceholder="Monthly" txtboxClass="no-border" inputClass="col xl-4 input-container" value="" />
+
+      <Input editable="false" inputLabel="Insured Name:" inputPlaceholder="LastN, FirstN, MName" txtboxClass="no-border" inputClass="col xl-10 input-container" value={insured && `${insured.clientLastName}, ${insured.clientFirstName}, ${insured.clientMiddleName}`}/>
+      <Input editable="false" inputLabel="Birthdate:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-2 input-container" value={insured && insured.dateOfBirth}/>
+      <Input editable="false" inputLabel="Owner Name:" inputPlaceholder="LastN, FirstN, MName" txtboxClass="no-border" inputClass="col xl-10 input-container" value={owner && `${owner.clientLastName}, ${owner.clientFirstName}, ${owner.clientMiddleName}`}/>
+      <Input editable="false" inputLabel="Birthdate:" inputPlaceholder="mm/dd/yyyy" txtboxClass="no-border" inputClass="col xl-2 input-container" value={owner && owner.dateOfBirth}/>
+    </div>
     );
   }
 }
